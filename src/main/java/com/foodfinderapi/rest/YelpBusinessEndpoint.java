980165e57cf4469fe6e24b2e7b324273065095c6
Patrick Hayes <ptman16@gmail.com>
2017-01-08 19:42:15 -0600

    error handling when location can't be found

--- src/main/java/com/foodfinderapi/rest/YelpBusinessEndpoint.java
+++ src/main/java/com/foodfinderapi/rest/YelpBusinessEndpoint.java
@@ -18,8 +18,10 @@ public class YelpBusinessEndpoint {
     @GET
     @Path("/{latitude}/{longitude}/{mileRadius}/{searchTerm}")
     @Produces(MediaType.APPLICATION_JSON)
-    public YelpBusiness[] get(@PathParam("latitude") double latitude, @PathParam("longitude") double longitude,
-                              @PathParam("mileRadius") int mileRadius, @PathParam("searchTerm") String searchTerm){
+    public YelpBusiness[] get(@PathParam("latitude") double latitude, @PathParam("longitude") double longitude, @PathParam("mileRadius") int mileRadius, @PathParam("searchTerm") String searchTerm){
+		if (latitude == 0.0 && longitude == 0.0)
+            return new YelpBusiness[0];
+	
         YelpAPI api = new YelpAPI();
         String response = api.Search(latitude, longitude, mileRadius, searchTerm);
         YelpParser parser = new YelpParser();
